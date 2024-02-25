4f85e3ebda03602f6d167781ef95b843ba0c71b7
kritz <jislavjj@gmail.com>
2024-02-25 14:03:13 +0200

    Added the first 2 problems from Objects and Classes - Exercises.

---
+++ ObjectsAndClasses01AdvertisementMessage/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Security.Cryptography;
+
+namespace ObjectsAndClasses01AdvertisementMessage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> phrases = new List<string>()
+            { 
+                "Excellent product.",
+                "Such a great product.",
+                "I always use that product.",
+                "Best product of its category.",
+                "Exceptional product.",
+                "I can't live without this product."
+            };
+
+            List<string> events = new List<string>()
+            {
+                "Now I feel good.",
+                "I have succeeded with this product.",
+                "Makes miracles. I am happy of the results!",
+                "I cannot believe but now I feel awesome.",
+                "Try it yourself, I am very satisfied.",
+                "I feel great!"
+            };
+
+            List<string> authors = new List<string>()
+            { 
+                "Diana",
+                "Petya",
+                "Stella",
+                "Elena",
+                "Katya",
+                "Iva",
+                "Annie",
+                "Eva"
+            };
+            List<string> cities = new List<string>()
+            {
+                "Burgas",
+                "Sofia",
+                "Plovdiv",
+                "Varna",
+                "Ruse"
+            };
+
+            int lines = int.Parse(Console.ReadLine());
+
+            Random randomNumber = new Random();
+
+           
+
+            for (int i = 0; i < lines; i++)
+            {
+                int randomIndex = 0;
+
+                randomIndex = GenerateRandomIndex(randomNumber, phrases);
+                Console.Write($"{phrases[randomIndex]} ");
+
+                randomIndex = GenerateRandomIndex(randomNumber, events);
+                Console.Write($"{events[randomIndex]} ");
+
+                randomIndex = GenerateRandomIndex(randomNumber, authors);
+                Console.Write($"{authors[randomIndex]} - ");
+
+                randomIndex = GenerateRandomIndex(randomNumber, cities);
+                Console.Write($"{cities[randomIndex]}. {Environment.NewLine}");
+                
+            }
+        }
+
+         static int GenerateRandomIndex(Random randomNumber, List<string> currentList)
+        {
+            int result = randomNumber.Next(currentList.Count);
+            return result;
+        }
+    }
+}
---
+++ ObjectsAndClasses02Articles/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace ObjectsAndClasses02Articles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+
+            string[] tokens = line.Split(", ");
+
+            string title = tokens[0];
+            string content = tokens[1];
+            string author = tokens[2];
+
+            Article article = new Article(title, content, author);
+
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfCommands ; i++)
+            {
+
+                string command = Console.ReadLine();
+                string[] commandTokens = command.Split(":");
+                command = commandTokens[0];
+                switch(command)
+                {
+                    case "Edit":
+                        article.Edit(article, commandTokens[1]);
+                        break;
+                    case "ChangeAuthor":
+                        article.ChangeAuthor(article, commandTokens[1]);
+                        break;
+                    case "Rename":
+                       article.Rename(article, commandTokens[1]);
+                        break;
+                }
+            }
+
+            Console.WriteLine($"{article.Title} - {article.Content}: {article.Author}");
+        }
+
+        //public void Rename(Article article, string newName)
+        //{
+        //    article.Author = newName;
+        //}
+
+        //public void ChangeAuthor(Article article, string author)
+        //{
+        //    article.Author = author;
+        //}
+
+        //public void Edit(Article article, string edit)
+        //{
+        //    article.Title = edit;
+        //}
+    }
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+       
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public void Rename(Article article, string newName)
+        {
+            article.Title = newName.Trim();
+        }
+        public void ChangeAuthor(Article article, string author)
+        {
+            article.Author = author.Trim();
+        }
+        public void Edit(Article article, string edit)
+        {
+            article.Content = edit.Trim();
+        }
+    }
+}
